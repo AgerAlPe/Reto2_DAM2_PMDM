fb8a7c1a0fd278d63f794f7e6ee13168c2d5bf08
AgerAlPe <ager.algortape@elorrieta-errekamari.com>
2024-01-09 17:15:01 +0100

    comprovación internet

--- app/src/main/java/com/grupo2/elorchat/MainActivity.kt
+++ app/src/main/java/com/grupo2/elorchat/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.grupo2.elorchat
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var connection : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.loginButton).setOnClickListener{
+            connection = isOnline(this)
+
+            if (connection) {
+                Toast.makeText(this, "there is connection", Toast.LENGTH_LONG).show()
+            }else {
+                Toast.makeText(this, "there is NO connection", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+        return false
     }
 }
