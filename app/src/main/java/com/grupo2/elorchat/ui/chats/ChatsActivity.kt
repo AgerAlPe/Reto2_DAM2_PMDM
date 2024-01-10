2e60733790cbc96b68b788c8ea5e878ff9021bd5
AgerAlPe <ager.algortape@elorrieta-errekamari.com>
2024-01-10 21:22:59 +0100

    miercoles de trabajo

--- app/src/main/java/com/grupo2/elorchat/ui/chats/ChatsActivity.kt
+++ app/src/main/java/com/grupo2/elorchat/ui/chats/ChatsActivity.kt
@@ -2,11 +2,52 @@ package com.grupo2.elorchat.ui.chats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.grupo2.elorchat.R
 
 class ChatsActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: FragmentPageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chats)
+
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager2 = findViewById(R.id.viewpager)
+
+        adapter = FragmentPageAdapter(supportFragmentManager, lifecycle)
+
+        tabLayout.addTab(tabLayout.newTab().setText("First"))
+        tabLayout.addTab(tabLayout.newTab().setText("Second"))
+        tabLayout.addTab(tabLayout.newTab().setText("Third"))
+
+        viewPager2.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
     }
 }
--- app/src/main/java/com/grupo2/elorchat/ui/users/register/RegisterActivity.kt
+++ app/src/main/java/com/grupo2/elorchat/ui/users/register/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.grupo2.elorchat.ui.users.register
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.grupo2.elorchat.R
+import com.grupo2.elorchat.ui.chats.ChatsActivity
 
 class RegisterActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
     }
 }
