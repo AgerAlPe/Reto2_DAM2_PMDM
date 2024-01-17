6e360dc10b557c6403b12150ccc20fcbc3e16f59
AnderLH <ander.lopezdevallejohi@elorrieta-errekamari.com>
2024-01-17 16:39:41 +0100

    hilt hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,11 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     //dagger hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
--- app/src/main/java/com/grupo2/elorchat/data/database/MessageDatabase.kt
+++ app/src/main/java/com/grupo2/elorchat/data/database/MessageDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase
 import com.grupo2.elorchat.data.database.dao.MessageDao
 import com.grupo2.elorchat.data.database.entities.MessageEntity
 
-@Database(entities = [MessageEntity::class], version = 1)
+@Database(entities = [MessageEntity::class], version = 1, exportSchema = false)
 abstract class MessageDatabase:RoomDatabase() {
 
     abstract fun getMessageDao():MessageDao
