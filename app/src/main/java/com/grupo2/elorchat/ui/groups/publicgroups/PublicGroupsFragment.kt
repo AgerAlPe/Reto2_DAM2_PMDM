44e634a3421841158ea4de925555f13d639736dd
AgerAlPe <ager.algortape@elorrieta-errekamari.com>
2024-01-22 17:27:55 +0100

    cambios en el socket activity

--- app/src/main/java/com/grupo2/elorchat/ui/groups/publicgroups/PublicGroupsFragment.kt
+++ app/src/main/java/com/grupo2/elorchat/ui/groups/publicgroups/PublicGroupsFragment.kt
@@ -66,7 +66,7 @@ class PublicGroupsFragment : Fragment() {
 
     private fun onGroupsListClickItem(group: Group) {
         val intent = Intent(requireContext(), SocketActivity::class.java).apply {
-            putExtra("idGroup", group.id)
+            putExtra("idGroup", group.id.toString())
         }
         startActivity(intent)
     }
