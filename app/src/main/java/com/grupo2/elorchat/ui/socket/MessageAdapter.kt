ba8afd2029b8356e2cbe1310fa968f0c3bbcd606
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2024-01-25 17:56:48 +0100

    adapter for Message on socket updated

--- app/src/main/java/com/grupo2/elorchat/ui/socket/MessageAdapter.kt
+++ app/src/main/java/com/grupo2/elorchat/ui/socket/MessageAdapter.kt
@@ -8,11 +8,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.grupo2.elorchat.data.Message
 import com.grupo2.elorchat.data.SocketMessage
 import com.grupo2.elorchat.databinding.ItemMessageBinding
+import com.grupo2.elorchat.databinding.MessageBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MessageAdapter : ListAdapter<Message, MessageAdapter.MessageViewHolder>(MessageDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        val binding = ItemMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = MessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MessageViewHolder(binding)
     }
 
@@ -21,12 +25,14 @@ class MessageAdapter : ListAdapter<Message, MessageAdapter.MessageViewHolder>(Me
         holder.bind(message)
     }
 
-    inner class MessageViewHolder(private val binding: ItemMessageBinding) :
+    inner class MessageViewHolder(private val binding: MessageBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(message: Message) {
-            binding.textViewMessage.text = message.message
-            binding.textViewAuthor.text = message.name
+            binding.messageText.text = message.message
+            binding.MessagerName.text = message.name
+            val dateString = convertTimestampToDate(message.createdAt)
+            binding.messageDate.text = dateString
             // You can bind other properties as needed
         }
     }
@@ -44,6 +50,21 @@ class MessageAdapter : ListAdapter<Message, MessageAdapter.MessageViewHolder>(Me
                     oldItem.chatId == newItem.chatId &&
                     oldItem.createdAt == newItem.createdAt
             // Add other properties as needed
+        }
+    }
+
+    fun convertTimestampToDate(timestamp: Long?): String {
+        if (timestamp == null) {
+            return ""
+        }
+
+        try {
+            val sdf = SimpleDateFormat("MMM dd, HH:mm:ss", Locale.getDefault())
+            val netDate = Date(timestamp)
+            return sdf.format(netDate)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ""
         }
     }
 }
