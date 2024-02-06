73eef7b37e8ad1479e9940cfb8340a9270027cb9
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2024-02-06 19:23:24 +0100

    Photos and Coordinates done

--- app/src/main/java/com/grupo2/elorchat/ui/socket/SocketActivity.kt
+++ app/src/main/java/com/grupo2/elorchat/ui/socket/SocketActivity.kt
@@ -1,9 +1,18 @@
 package com.grupo2.elorchat.ui.socket
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
@@ -14,6 +23,7 @@ import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.grupo2.elorchat.R
@@ -31,7 +41,12 @@ import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
+import java.io.ByteArrayOutputStream
+import java.util.Base64
 import javax.inject.Inject
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import androidx.core.app.ActivityCompat
+
 
 @AndroidEntryPoint
 class SocketActivity() : ComponentActivity() {
@@ -50,6 +65,15 @@ class SocketActivity() : ComponentActivity() {
 
     private lateinit var socketService: SocketService
     private var isBind = false
+
+    private val PICK_IMAGE_REQUEST = 1
+    private var base64Image: String? = null
+
+    private val PICK_FILE_REQUEST = 2
+
+    private lateinit var locationManager: LocationManager
+
+
 
     @Inject
     lateinit var appDatabase: AppDatabase
@@ -73,6 +97,10 @@ class SocketActivity() : ComponentActivity() {
         }
 
         val addMessageImageView: ImageView = findViewById(R.id.addMessageImageView)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+
 
         addMessageImageView.setOnClickListener { view ->
             showPopupMenu(view)
@@ -107,22 +135,29 @@ class SocketActivity() : ComponentActivity() {
         Log.d("ButtonClickListener", "Connect button clicked")
     }
 
-    private fun showPopupMenu(view: View) {
+    fun showPopupMenu(view: View) {
         val popupMenu = PopupMenu(this, view)
         popupMenu.menuInflater.inflate(R.menu.message_type, popupMenu.menu)
 
         popupMenu.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.send_photo -> {
-                    // Lógica para enviar una foto
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.type = "image/*"
+                    startActivityForResult(intent, PICK_IMAGE_REQUEST)
                     return@setOnMenuItemClickListener true
                 }
                 R.id.send_coordinates -> {
-                    // Lógica para enviar coordenadas GPS
+                    sendCoordinates { coordinates ->
+                        // Send the coordinates as the message content
+                        socketService.sendMessage(coordinates, groupName)
+                    }
                     return@setOnMenuItemClickListener true
                 }
                 R.id.send_file -> {
-                    // Lógica para enviar un archivo
+                    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                    intent.type = "*/*"
+                    startActivityForResult(intent, PICK_FILE_REQUEST)
                     return@setOnMenuItemClickListener true
                 }
                 else -> return@setOnMenuItemClickListener false
@@ -177,13 +212,23 @@ class SocketActivity() : ComponentActivity() {
 
     private fun buttonsListeners(binding: ActivitySocketBinding) {
         binding.btnSendMessage.setOnClickListener {
-            Log.d("ButtonClickListener", "Send Message button clicked")
-            val message = binding.inputMessage.text.toString()
-            binding.inputMessage.setText("")
-            // viewModel.onSendMessage(message)
+        Log.d("ButtonClickListener", "Send Message button clicked")
+        val messageContent = binding.inputMessage.text.toString()
+        binding.inputMessage.setText("")
+        // Store base64Image in a local variable
+        val localBase64Image = base64Image
+        // Reset the base64Image after storing it
+        base64Image = null
 
-            socketService.sendMessage(message, groupName)
+        // Check if an image is selected
+        if (localBase64Image != null) {
+            // Send the message with the image data
+            socketService.sendMessage(localBase64Image, groupName)
+        } else {
+            // Send the message without the image data
+            socketService.sendMessage(messageContent, groupName)
         }
+    }
 
         binding.leaveButton.setOnClickListener {
             Log.d("ButtonClickListener", "Leave Chat button clicked")
@@ -191,7 +236,7 @@ class SocketActivity() : ComponentActivity() {
             lifecycleScope.launch {
                 try {
                     val userId = appDatabase.getUserDao().getAllUser().first().id
-                  
+
                     groupId?.let { it1 -> groupViewModel.leaveChat(userId!!, it1) }
 
                     socketViewModelt.leaveRoom(groupName, userId!!)
@@ -224,6 +269,86 @@ class SocketActivity() : ComponentActivity() {
 
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                PICK_IMAGE_REQUEST -> {
+                    // Handle image selection
+                    val imageUri = data?.data
+                    val inputStream = imageUri?.let { contentResolver.openInputStream(it) }
+                    val bitmap = BitmapFactory.decodeStream(inputStream)
+                    inputStream?.close()
+
+                    if (bitmap != null) {
+                        val base64Image = bitmapToBase64(bitmap)
+                        // Send the message with the image data
+                        socketService.sendMessage(base64Image, groupName)
+                    } else {
+                        // Handle the case where bitmap is null
+                        Toast.makeText(this, "Failed to decode image data", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                PICK_FILE_REQUEST -> {
+                    // Handle file selection
+                    val fileUri = data?.data
+                    val inputStream = fileUri?.let { contentResolver.openInputStream(it) }
+                    val byteArray = inputStream?.readBytes()
+                    inputStream?.close()
+
+                    if (byteArray != null) {
+                        val base64File = Base64.getEncoder().encodeToString(byteArray)
+                        // Send the message with the file data
+                        socketService.sendMessage(base64File, groupName)
+                    } else {
+                        // Handle the case where byteArray is null
+                        Toast.makeText(this, "Failed to read file data", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun bitmapToBase64(bitmap: Bitmap): String {
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 20, outputStream)
+        val byteArray = outputStream.toByteArray()
+        return Base64.getEncoder().encodeToString(byteArray)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun sendCoordinates(callback: (String) -> Unit) {
+        // Check if permission is granted
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Request location updates
+            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, object :
+                LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    // Send the coordinates
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    val coordinatesMessage = "Latitude: $latitude, Longitude: $longitude"
+                    // Pass the coordinates message to the callback function
+                    callback.invoke(coordinatesMessage)
+                }
+
+                override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+                override fun onProviderEnabled(provider: String) {}
+                override fun onProviderDisabled(provider: String) {}
+            }, null)
+        } else {
+            // Permission not granted, request it from the user
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1001
+    }
 
     override fun onStart() {
         super.onStart()
