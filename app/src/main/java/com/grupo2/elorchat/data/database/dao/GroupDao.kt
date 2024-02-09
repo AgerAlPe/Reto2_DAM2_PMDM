86f48e8bcd2b5b265ed3d4a51e48ebe93f103ed0
Galder0 <galder.gonzalez-balsiz@elorrieta-errekamari.com>
2024-02-09 19:31:50 +0100

    delete error fixed

--- app/src/main/java/com/grupo2/elorchat/data/database/dao/GroupDao.kt
+++ app/src/main/java/com/grupo2/elorchat/data/database/dao/GroupDao.kt
@@ -26,8 +26,8 @@ interface GroupDao {
     @Query("SELECT * FROM group_table WHERE id = :groupId")
     suspend fun getGroupById(groupId: Int): GroupEntity?
 
-    @Query("Delete FROM group_table WHERE id = :groupId")
-    suspend fun deleteGroup(groupId: Int): GroupEntity?
+    @Query("DELETE FROM group_table WHERE id = :groupId")
+    suspend fun deleteGroup(groupId: Int)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertGroup(group: GroupEntity)
